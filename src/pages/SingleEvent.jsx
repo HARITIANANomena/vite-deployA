4b74371be134f8ae7ce2ad2689dc9b1b0413ce70
Haritiana <haritiananomenjanahary3@gmail.com>
2024-01-25 11:03:05 +0300

    Ajout section event

--- src/pages/SingleEvent.jsx
+++ src/pages/SingleEvent.jsx
@@ -4,8 +4,11 @@ export default function SingleEvent() {
   return (
     <>
       <div className="ctn_single_evenement h-screen bg-yellow-300">
-        <div>SingleEvent</div>
-        
+        <div className="h-64 bg-gray-300">
+          SingleEvent</div>
+        <div className="ctn_table">
+            Une table
+        </div>
       </div>
     </>
   )
